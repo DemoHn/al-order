c539430e84d403666f1a22f2de831a0316fc74ee
DemoHn <demohn2016@gmail.com>
2019-08-08 11:26:02 +0800

    feat: update order table

--- sql/up.sql
+++ sql/up.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS orders (
-  id INT AUTO_INCREMENT,
+  sequence_id INT AUTO_INCREMENT,
+  id VARCHAR(50),
   status VARCHAR(255) NOT NULL,
   distance FLOAT(10, 2),
-  origin_lat FLOAT(10, 2) NOT NULL,
-  origin_lng FLOAT(10, 2) NOT NULL,
-  dest_lat FLOAT(10, 2) NOT NULL,
-  dest_lng FLOAT(10, 2) NOT NULL,
-  PRIMARY KEY (id)
+  location_info TEXT NOT NULL,
+  PRIMARY KEY (sequence_id),
+  UNIQUE (id)
 );
