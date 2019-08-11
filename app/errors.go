993ddb0cd0dd991f3954763dc7dba47b5b2e6530
DemoHn <demohn2016@gmail.com>
2019-08-11 15:45:54 +0800

    feat: finish order servicesw

--- app/errors.go
+++ app/errors.go
@@ -2,18 +2,68 @@ package app
 
 // Error - customized AppError for displaying error response
 type Error struct {
-	Name       string
+	Message    string
 	StatusCode int
 	Code       int
 	Details    string
 }
 
 // ErrIDNotFound - if input ID is empty
-func ErrIDNotFound(details string) Error {
-	return Error{
-		Name:       "ErrIDNotFound",
+func ErrIDNotFound(details string) *Error {
+	return &Error{
+		Message:    "orderID Not Found",
 		StatusCode: 400,
 		Code:       10000,
 		Details:    details,
+	}
+}
+
+// ErrDBFatal - db operation error
+func ErrDBFatal(err error) *Error {
+	return &Error{
+		Message:    "fatal error: db operation error",
+		StatusCode: 500,
+		Code:       10001,
+		Details:    err.Error(),
+	}
+}
+
+// ErrRedisFatal - redis operation error
+func ErrRedisFatal(err error) *Error {
+	return &Error{
+		Message:    "fatal error: redis operation error",
+		StatusCode: 500,
+		Code:       10002,
+		Details:    err.Error(),
+	}
+}
+
+// ErrGoogleMapService - googleMap service error
+func ErrGoogleMapService(err error) *Error {
+	return &Error{
+		Message:    "fetch google map direction data failed",
+		StatusCode: 500,
+		Code:       10003,
+		Details:    err.Error(),
+	}
+}
+
+// ErrNoRoute - if there's no available route from origin -> dest
+func ErrNoRoute() *Error {
+	return &Error{
+		Message:    "there's no available route from origin -> destination",
+		StatusCode: 400,
+		Code:       10004,
+		Details:    "",
+	}
+}
+
+// ErrOrderHasTaken -
+func ErrOrderHasTaken() *Error {
+	return &Error{
+		Message:    "order has been taken by somebody else",
+		StatusCode: 400,
+		Code:       10005,
+		Details:    "",
 	}
 }
--- app/srv_order.go
+++ app/srv_order.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
+	"time"
 
 	util "github.com/DemoHn/al-order/util"
 	"github.com/go-redis/redis"
@@ -15,23 +17,135 @@ var (
 	dFindOneOrder      func(*sql.DB, string) (*Order, error)
 	dUpdateOrderStatus func(*sql.DB, string, OrderStatus) error
 	dListOrders        func(*sql.DB, int, int) ([]Order, error)
+	rAddOrderLock      func(*redis.Client, string) (bool, error)
+	rDelOrderLock      func(*redis.Client, string) error
 	gdb                *sql.DB
 	gredis             *redis.Client
 )
 
-// PlaceOrder - place a new order
-func PlaceOrder() Error {
+// types
 
+// LocationInput - location input request schema
+type LocationInput struct {
+	Origin      []string `json:"origin"`
+	Destination []string `json:"destination"`
+}
+
+// OrderResponse - show the response of order
+type OrderResponse struct {
+	ID       string `json:"id"`
+	Distance int    `json:"distance"`
+	Status   string `json:"status"`
+}
+
+// PlaceOrder - place a new order
+func PlaceOrder(input LocationInput) (*OrderResponse, *Error) {
+	var loc = LocationInfo{
+		StartLat: input.Origin[0],
+		StartLng: input.Origin[1],
+		EndLat:   input.Destination[0],
+		EndLng:   input.Destination[1],
+	}
+	db, err := getDB()
+	if err != nil {
+		return nil, ErrDBFatal(err)
+	}
+	orderID := newUUID()
+
+	// 01. get distance data from GoogleMap direction API
+	hasRoute, distance, err := GetRouteDistance(loc)
+	if err != nil {
+		return nil, ErrGoogleMapService(err)
+	}
+	if hasRoute == false {
+		return nil, ErrNoRoute()
+	}
+
+	// 02. write down data
+	if err := dSaveNewOrder(db, orderID, distance, loc); err != nil {
+		return nil, ErrDBFatal(err)
+	}
+
+	// 03. find & return the final data
+	order, err := dFindOneOrder(db, orderID)
+	if err != nil {
+		return nil, ErrDBFatal(err)
+	}
+
+	return transformOrderResp(order), nil
 }
 
 // TakeOrder - take an existing order
-func TakeOrder() {
+func TakeOrder(orderID string) *Error {
+	r := getRedis()
+	// 01. add lock to avoid unexpected race-condition - that
+	// both two users take the order at the same time
+	lockResult, err := rAddOrderLock(r, orderID)
+	if err != nil {
+		return ErrRedisFatal(err)
+	}
+	defer rDelOrderLock(r, orderID)
+	if lockResult == false {
+		return ErrOrderHasTaken()
+	}
 
+	// 02. find if orderID exists
+	db, err := getDB()
+	if err != nil {
+		return ErrDBFatal(err)
+	}
+	order, err := dFindOneOrder(db, orderID)
+	if err != nil {
+		return ErrDBFatal(err)
+	}
+	if order == nil {
+		return ErrIDNotFound(fmt.Sprintf("order id: %s not found", orderID))
+	}
+	// 03. check if orderID status is still unassigned
+	if order.Status != Unassigned {
+		return ErrOrderHasTaken()
+	}
+	// 04. so it's time to update the status!
+	if err := dUpdateOrderStatus(db, orderID, Taken); err != nil {
+		return ErrDBFatal(err)
+	}
+	return nil
 }
 
 // ListOrder - list recorded orders
-func ListOrder() {
+func ListOrder(page *int, limit *int) (*[]OrderResponse, *Error) {
+	var rPage, rLimit int
+	// get page, limit
+	if page == nil {
+		rPage = 1
+	} else {
+		rPage = *page
+	}
 
+	if limit == nil {
+		rLimit = 10
+	} else {
+		rLimit = *limit
+	}
+
+	var rOffset = (rPage - 1) * rLimit
+
+	// 01. list orders
+	db, err := getDB()
+	if err != nil {
+		return nil, ErrDBFatal(err)
+	}
+	orders, err := dListOrders(db, rLimit, rOffset)
+	if err != nil {
+		return nil, ErrDBFatal(err)
+	}
+
+	// 02. transform orders
+	var tOrders = make([]OrderResponse, len(orders))
+	for index, order := range orders {
+		tOrders[index] = *transformOrderResp(&order)
+	}
+	return &tOrders, nil
 }
 
 // private functions
@@ -61,10 +175,34 @@ func getRedis() *redis.Client {
 	})
 }
 
+func transformOrderResp(order *Order) *OrderResponse {
+	return &OrderResponse{
+		ID:       order.ID,
+		Status:   order.Status,
+		Distance: int(order.Distance),
+	}
+}
+
+// redis helper
+func addOrderLock(client *redis.Client, orderID string) (bool, error) {
+	key := fmt.Sprintf("order_lock:%s", orderID)
+	return client.SetNX(key, "1", 10*time.Second).Result()
+}
+
+func deleteOrderLock(client *redis.Client, orderID string) error {
+	key := fmt.Sprintf("order_lock:%s", orderID)
+	if _, err := client.Del(key).Result(); err != nil {
+		return err
+	}
+	return nil
+}
+
 // init function
 func init() {
 	dSaveNewOrder = SaveNewOrder
 	dFindOneOrder = FindOneOrder
 	dUpdateOrderStatus = UpdateOrderStatus
 	dListOrders = ListOrders
+	rAddOrderLock = addOrderLock
+	rDelOrderLock = deleteOrderLock
 }
